3414d34091afe33a59b55541e710ed5a94685da1
Markusas <saltinio@gmail.com>
2019-04-02 13:24:59 +0300

    Updated data base

--- payment/src/main/resources/data.sql
+++ payment/src/main/resources/data.sql
@@ -4,3 +4,14 @@ insert into payment
 insert into payment
      values (2, '100.60', '2012-12-03', '1');
 
+insert into payment
+     values (3, '1600.70', '2013-12-03', '1');
+
+insert into payment
+     values (4, '2350.22', '2014-12-03', '1');
+
+insert into payment
+     values (5, '10.90', '2012-06-01', '2');
+
+insert into payment
+     values (6, '756.80', '2012-07-01', '2');
--- person/src/main/resources/data.sql
+++ person/src/main/resources/data.sql
@@ -1,3 +1,6 @@
 insert into person
     values (1, 'Tadas', '39205050120');
 
+insert into person
+    values (2, 'Martynas', '38902300120');
+
